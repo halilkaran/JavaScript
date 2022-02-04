f540eeebe1053a52f608e467c690bab59a85f7e6
Halil <78084765+halilkaran@users.noreply.github.com>
2022-02-04 10:23:00 +0100

    session4

--- class-notes/S04_Fonksiyonlar/fonksiyon.js
+++ class-notes/S04_Fonksiyonlar/fonksiyon.js
@@ -2,6 +2,22 @@
 // ?                    FONKSİYONLAR
 // ? ==========================================================
 
+
+
+ 
+console.log("wqqeq");
+let a = 5;
+const tekMi = function (x) {
+    a = 10;
+    const c = 12;
+    return x % 2 ? "tek" : "cift";
+}
+console.log(tekMi(4)); 
+
+const kare = (x) => x * x;
+console.log(kare(4));
+
+console.log(c);
 // !-----------------------------------------------------------
 // ! 1.YÖNTEM  : FUNCTION DECLARATION
 // !-----------------------------------------------------------
