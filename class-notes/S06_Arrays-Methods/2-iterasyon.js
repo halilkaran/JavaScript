bf55e77dbe88bec4942c8f2903c171caeb14b6d2
Halil <78084765+halilkaran@users.noreply.github.com>
2022-02-07 19:44:41 +0100

    session-6

---
+++ class-notes/S06_Arrays-Methods/2-iterasyon.js
@@ -0,0 +1,52 @@
+//* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//*                        DİZİLERDE İTERASYON
+//* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//* ======================================================
+//*                     FOR DÖNGÜSÜ
+//* ======================================================
+
+const diziIsaretliTopla = (dizi) => {
+  let sumP = 0,
+    sumN = 0;
+  for (let i = 0; i < dizi.length; i++) {
+    if (dizi[i] > 0) {
+      sumP += dizi[i];
+    } else {
+      sumN += dizi[i];
+    }
+  }
+  return "Negatiflerin Toplami=" + sumN + " Pozitiflerin Toplami=" + sumP;
+};
+
+const koordinatlar = [120, 100, -100, 220, 330, -40];
+console.log(diziIsaretliTopla(koordinatlar));
+
+//* ======================================================
+//*                   FOR IN DÖNGÜSÜ
+//* ======================================================
+
+//*-------------------------------------------------------
+//* SORU: Bir hayvanat bahçesinde bulunan hayvan türlerinin
+//* bir dizide saklandığı varsayalım.Bu hayvanları türüne
+//* göre aramamızı sağlayacak ve o türden kaç adet bulunduğu
+//*  ana programa döndürecek fonksiyonu yazınız. Eğer o
+//* türden bir hayvan yok ise bulunamadığını yazdırsın.
+//*--------------------------------------------------------
+
+//* ÖDEV
+//*----------------------------------------------------------
+
+
+//* ======================================================
+//*                   FOR OF DÖNGÜSÜ
+//* ======================================================
+
+//* for of dongusu, for in dongusunun bir cok veri yapisini kapsayacak sekilde guncellenmis halidir. Bu dongude dizi icersindeki veriye erisirken indisleme kullanmaya gerek yoktur.
+let araclar = ["BMW", "Volvo", "Mini"];
+let yazi = "";
+for (let arac of araclar) {
+  console.log(arac);
+  yazi += arac + " "; // arac:  dizinin her bir elemanini temsil etmektedir.
+}
+console.log(yazi);
